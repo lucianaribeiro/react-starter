547a641c5ed6aa74cec53c7553e9985d8eed7701
Luciana Ribeiro <luribeiro@pop-os.localdomain>
2019-07-28 23:48:29 -0300

    setting up a react starter projet with a simple example using state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      name: 'Luciana'
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    this.setState({
+      name: this.state.name === 'Luciana' ? 'mudou' : 'Luciana'
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.name}</p> 
+        <button type="button" onClick={this.handleClick}>trocar</button>       
+      </div>
+    );
+  }
 }
 
 export default App;
